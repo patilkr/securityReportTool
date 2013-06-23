07d4de37a52e533d8e49a64a79636b5a94eedce1
Kailas <patilkr@patilkr-K43SD.(none)>
2013-06-23 19:16:20 +0530

    created UI for toolbox panel and added modules

---
+++ data/securityReport.js
@@ -0,0 +1,46 @@
+/*
+ *
+ * ToolName: Security Report Tool
+ * File Purpose: Regiter ToolBox panel and GCLI commands
+ * Author: PATIL Kailas
+ *
+ */
+
+
+// Supplimentary function to change class of tab
+function changeDirectiveClass(id, flag) {
+    switch (id) {
+        case 1:
+            if (!flag)
+                document.getElementById("csp").className = "";
+            else
+                document.getElementById("csp").className = "current";
+            break;
+        case 2:
+            if (!flag)
+                document.getElementById("cors").className = "";
+            else
+                document.getElementById("cors").className = "current";
+            break;
+        case 3:
+            if (!flag)
+                document.getElementById("mixed-content").className = "";
+            else
+                document.getElementById("mixed-content").className = "current";
+            break;
+    }
+} // end of changeDirectiveClass() function
+
+// Change log of tab contents
+function changeReportLogs(evt, curTabId) {
+    if (previousTabId === curTabId) return; 
+        
+    // Remove "current" class from oldDirective
+    changeDirectiveClass(previousTabId, false);
+    // Set "current" class to currently selected tab
+    changeDirectiveClass(curTabId, true);
+    // Store curTabID for next reference
+    previousTabId = curTabId;    
+
+} // end of changeReportLogs() function
+
