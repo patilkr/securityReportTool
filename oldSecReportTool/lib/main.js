1a5e21a329031c177c77c8f9ea6e43f564ccb47b
Kailas <patilkr@patilkr-K43SD.(none)>
2013-07-10 18:51:08 +0530

    toolbox panel version of security tool backup copy created in oldSecReportTool

---
+++ oldSecReportTool/lib/main.js
@@ -0,0 +1,122 @@
+const {Cc,Ci,Cu} = require("chrome");
+
+const xpcom = require("xpcom");
+
+try { Cu.import("resource://gre/modules/Services.jsm"); } catch (e) { }
+try { Cu.import("resource://gre/modules/XPCOMUtils.jsm"); } catch (e) { }
+try { Cu.import("resource://gre/modules/NetUtil.jsm"); } catch (e) { }
+
+// Include our custom Modules
+// const securityReportUI = require("securityReportUI");
+
+/*
+* Code to intercept messages sent to Error Console (aka. Browser Console)
+*/
+var errorListener = {
+    observe: function(aMessage) {
+        // Get nsIScriptError object to retrieve "category" info of msg
+        let error = aMessage.QueryInterface(Ci.nsIScriptError);
+        
+        if (error instanceof Ci.nsIScriptError) {
+         //   dump("\n Message category:  "+error.category +"\n");
+         // Send captured message data for display in UI
+         securityReportUI.displayErrorMsg(error, aMessage);
+         //  dump("\n Content Script- Message category:  "+error.category +"\n");
+        }
+       
+        // dump("\n Message = "+aMessage.message);
+
+       // if (aMessage.message.contains("NS_ERROR"))
+          //  dump("NS_ERROR messages");
+    }
+};
+
+// Register event listener for console message service to captrue msg
+ var consoleService = Cc["@mozilla.org/consoleservice;1"].getService(Ci.nsIConsoleService);
+ consoleService.registerListener(errorListener);
+
+/*
+ * Observer registration on Security Errors and Warnings
+ * CSP observer event = "csp-on-violate-policy"
+ * mixed-content observer event =  ""
+ * SSL observer event =  ""
+ * CORS observer event =  ""
+ */
+var ConsoleAPIObserver = {
+  init: function init() {
+   dump("\n Init invoked!! \n");   
+   Services.obs.addObserver(this, "csp-on-violate-policy", false);
+   Services.obs.addObserver(this, "report-ssl-errors", false);
+   Services.obs.addObserver(this, "report-mixed-content", false);
+  },  
+
+  observe: function observe(aSubject, aTopic, aData) {
+      if (aTopic == "csp-on-violate-policy") {
+          dump("\n\n aTopic = " + aTopic);
+          dump("\n aData = " + aData);  
+          
+          try {
+              var uri = aSubject.QueryInterface(Ci.nsIURI);
+              if (uri instanceof Ci.nsIURI) {
+                  dump("\n aSubject.data = " + uri.asciiSpec);
+              }
+          } catch (e) {
+              //if that fails, the aSubject is probably a string
+              var str = aSubject.QueryInterface(Ci.nsISupportsCString);
+              dump("\n aSubject is a STRING!!! str = "+ str + "\n");
+          }
+      } else if (aTopic == "report-ssl-errors") {
+          dump("\n\n aTopic = " + aTopic);
+          dump("\n aData = " + aData);
+          dump("\n aSubject = " + aSubject);
+      }else if (aTopic == "report-mixed-content") {
+          dump("\n\n aTopic = " + aTopic);
+          dump("\n aData = " + aData);
+          try {
+              var uri = aSubject.QueryInterface(Ci.nsIURI);
+              if (uri instanceof Ci.nsIURI) {
+                  dump("\n aSubject.data = " + uri.asciiSpec);
+              }
+          } catch (e) {
+              //if that fails, the aSubject is probably a string
+              var str = aSubject.QueryInterface(Ci.nsISupportsCString);
+              dump("\n aSubject is a STRING!!! str = "+ str + "\n");
+          }
+      }               
+  } // end of observer function
+  
+}; // end of consoleAPIObserver object
+ConsoleAPIObserver.init();
+
+
+
+
+/*
+* ConsoleMessage and observer notification interception code Ends here
+*/
+// -----------------------------------------------------------------------
+
+
+
+/*
+* Code to add "Security Report" to Developer Tools Toolbox
+*
+* Code References:
+*  https://developer.mozilla.org/en-US/docs/Tools/DevToolsAPI
+*  https://developer.mozilla.org/en-US/docs/Social_API_Devtools
+*  http://mxr.mozilla.org/mozilla-aurora/source/browser/devtools/framework/test/browser_toolbox_dynamic_registration.js
+*/
+
+
+// Register ToolBox Panel
+// securityReportUI.registerSecurityReportTool();
+
+
+// --------------------------------------------------------------------
+// Add-on Unload Routine
+require("unload").when(function() { 
+    // Unregister security report tool
+    // securityReportUI.securityReportToolUnregister();
+});
+// --------------------------------------------------------------------
+
---
+++ oldSecReportTool/lib/securityReportUI.js
@@ -0,0 +1,163 @@
+/* 
+ * 
+ * ToolName: Security Report Tool
+ * File Purpose: Regiter ToolBox panel and GCLI commands
+ * Author: PATIL Kailas
+ * 
+ */
+
+/*
+ *  TODO List
+ *  1. Tab reloading should reset values in reportUI.CSP, CORS, etc
+ *  2. Tab switching should display correct contents
+ *  3. Icon of the Security Report Tool
+ *  4. Exit button click doesn't work
+ */
+
+const {Cc,Ci,Cu} = require("chrome");
+
+// Get Developers Tools 
+const { gDevTools } = Cu.import('resource:///modules/devtools/gDevTools.jsm', {}); // We can also use 'null' for {}
+const { Toolbox } = Cu.import('resource:///modules/devtools/Toolbox.jsm', {});
+const { TargetFactory } = Cu.import('resource:///modules/devtools/Target.jsm', {});
+
+/* Depending on the version of Firefox, promise module can have different path */
+try { Cu.import("resource://gre/modules/commonjs/promise/core.js"); } catch(e) {}
+try { Cu.import("resource://gre/modules/commonjs/sdk/core/promise.js"); } catch(e) {}
+
+ // Cu.import("resource:///modules/devtools/EventEmitter.jsm");
+
+// dump("\n\n gDevTools = "+ gDevTools);
+
+var data = require("self").data;
+
+exports.displayErrorMsg = function displayErrorMsg(aError, aMsg) {
+    
+    if (!reportUI.panelWin || typeof(reportUI.panelWin) === "undefined") return;
+        
+   // dump("\n Error Category = " + aError.category);
+   
+    if (aError.category === "Content Security Policy" || aError.category === "CSP") {
+        reportUI.CSP =  reportUI.CSP + aMsg.message + "\n";
+    }        
+    
+    switch (reportUI.uiState) {
+        case 1:
+            reportUI.panelWin.document.getElementById('logTextarea').value = reportUI.CSP; 
+            break;
+        case 2:
+            reportUI.panelWin.document.getElementById('logTextarea').value = reportUI.CORS; 
+            break;
+        case 3:
+            reportUI.panelWin.document.getElementById('logTextarea').value = reportUI.Mixed_content; 
+            break;
+        case 4:
+            reportUI.panelWin.document.getElementById('logTextarea').value = reportUI.SSL; 
+            break;
+    }
+   // if (reportUI.panelWin.document.getElementById("csp").className === "current") {
+  //  }
+
+};
+
+
+
+let reportUI = {
+    init: function(iframeWindow, aToolbox) {
+        this.toolbox = aToolbox;
+        this.panelWin = iframeWindow;
+        
+        this.uiState = 1;
+        // Error Msg type
+        this.CSP = "";
+        this.CORS = "";
+        this.Mixed_content = "";
+        this.SSL = "";
+        
+        this.content = this.toolbox.target.tab.linkedBrowser.contentWindow;
+        this._window = this.toolbox.target.tab.ownerDocument.defaultView;
+       // dump("\n\n\n this.content = "+ this.content);
+       // dump("\n this._window = "+ this._window);
+        // let parentDoc = iframeWindow.document.defaultView.parent.document;
+        // this.test();
+        this._window.addEventListener("unload", this.destroy, false);
+        
+        reportUI.panelWin.document.documentElement.addEventListener("addon-message", function(event) {
+            // dump("\n\n KRP1:"+event.detail);
+            reportUI.uiState = event.detail;
+            switch(event.detail) {
+            case 1: reportUI.panelWin.document.getElementById('logTextarea').value = reportUI.CSP;
+                break;
+            case 2: reportUI.panelWin.document.getElementById('logTextarea').value = reportUI.CORS;
+                break;
+            case 3: reportUI.panelWin.document.getElementById('logTextarea').value = reportUI.Mixed_content;
+                break;
+            case 4: reportUI.panelWin.document.getElementById('logTextarea').value = reportUI.SSL;
+                break;
+            }
+        }, false);
+    },
+    destroy: function() {
+        dump("\n\n\ Destroy funciton invoked!!\n\n");
+        this._window = null;
+        this.content = null;
+        return Promise.resolve(null);
+    },
+   
+}; // end of reportUI function
+
+
+exports.registerSecurityReportTool = function registerSecurityReportTool() {
+  let securityToolDefinition = {
+    id: "security-report-tool",
+    label: "Security Report",
+    icon: data.url("images/icon.png"),
+    url: data.url("securityReport.xhtml"),
+    tooltip: "Security Report Tool",
+    isTargetSupported: function(target) {
+       return !target.isRemote;
+    },
+    build: function(iframeWindow, toolbox) {
+         reportUI.init(iframeWindow, toolbox);
+         return Promise.resolve(iframeWindow.reportUI);       
+    }
+  };
+
+  gDevTools.registerTool(securityToolDefinition); // register ToolBox
+  addCommands(); // register GCLI commands
+} // end of registerSecurityReportTool() function
+
+
+// UnRegister Security Report Tool from ToolBox
+exports.securityReportToolUnregister = function securityReportToolUnregister() {
+    gDevTools.unregisterTool("security-report-tool");
+    //dump("\n Security Report Tool is unregistered from ToolBox");
+    removeCommands();
+} // end of securityReportToolUnregister() function
+
+
+// Register GCLI commands for our tool
+function addCommands() {
+    Cu.import("resource:///modules/devtools/gcli.jsm");
+    
+  /*
+   * 'security-report' command.
+   */
+  gcli.addCommand({
+    name: "security-report",
+    description: "Control the security report tool using the following commands:"
+  });
+  // TODO --- Add more commands here
+  
+} // end of addCommands() function
+
+// Unregister GCLI commands of Our tool
+function removeCommands() {
+  gcli.removeCommand("security-report");
+  // TODO --- unregister of more GCLI commands goes here
+  
+} //end of removeCommands() function
+
+let myVariable = {
+    
+};
