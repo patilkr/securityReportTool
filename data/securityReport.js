d54c11d592a54fb73fced58e244788fcd2b3f554
Kailas <patilkr@patilkr-K43SD.(none)>
2013-09-12 11:41:02 +0530

    download report button added

--- data/securityReport.js
+++ data/securityReport.js
@@ -8,62 +8,66 @@
 
 // Supplimentary function to change class of tab
 function changeDirectiveClass(id, flag) {
-    switch (id) {
-        case 1:
-            if (!flag)
-                document.getElementById("csp").className = "";
-            else
-                document.getElementById("csp").className = "current";
-            break;
-        case 2:
-            if (!flag)
-                document.getElementById("cors").className = "";
-            else
-                document.getElementById("cors").className = "current";
-            break;
-        case 3:
-            if (!flag)
-                document.getElementById("mixed-content").className = "";
-            else
-                document.getElementById("mixed-content").className = "current";
-            break;
-        case 4:
-            if (!flag)
-                document.getElementById("ssl").className = "";
-            else
-                document.getElementById("ssl").className = "current";
-            break;
-        case 5:
-            if (!flag)
-                document.getElementById("sec-headers").className = "";
-            else
-                document.getElementById("sec-headers").className = "current";
-            break;
-                
-    }
+	switch (id) {
+		case 1:
+			if (!flag)
+				document.getElementById("csp").className = "";
+			else
+				document.getElementById("csp").className = "current";
+			break;
+		case 2:
+			if (!flag)
+				document.getElementById("insec-passwd").className = "";
+			else
+				document.getElementById("insec-passwd").className = "current";
+			break;
+		case 3:
+			if (!flag)
+				document.getElementById("mixed-content").className = "";
+			else
+				document.getElementById("mixed-content").className = "current";
+			break;
+		case 4:
+			if (!flag)
+				document.getElementById("ssl").className = "";
+			else
+				document.getElementById("ssl").className = "current";
+			break;
+		case 5:
+			if (!flag)
+				document.getElementById("sec-headers").className = "";
+			else
+				document.getElementById("sec-headers").className = "current";
+			break;
+
+	}
 } // end of changeDirectiveClass() function
 
 // Change log of tab contents
 function changeReportLogs(evt, curTabId) {
-    if (previousTabId === curTabId) return; 
-        
-    // Remove "current" class from oldDirective
-    changeDirectiveClass(previousTabId, false);
-    // Set "current" class to currently selected tab
-    changeDirectiveClass(curTabId, true);
-    // Store curTabID for next reference
-    previousTabId = curTabId;    
+	if (previousTabId === curTabId)
+		return;
 
-    // Set textarea value to Empty
-    document.getElementById('logTextarea').value = "";
+	// Remove "current" class from oldDirective
+	changeDirectiveClass(previousTabId, false);
+	// Set "current" class to currently selected tab
+	changeDirectiveClass(curTabId, true);
+	// Store curTabID for next reference
+	previousTabId = curTabId;
 
-    // window.postMessage("Message from page script", "Hello World!");
+	// Set textarea value to Empty
+	document.getElementById('logTextarea').value = "";
 
-    var event = document.createEvent('CustomEvent');
-    event.initCustomEvent("addon-message", true, true, curTabId);
-    document.documentElement.dispatchEvent(event);
+	// window.postMessage("Message from page script", "Hello World!");
+
+	var event = document.createEvent('CustomEvent');
+	event.initCustomEvent("addon-message", true, true, curTabId);
+	document.documentElement.dispatchEvent(event);
 
 } // end of changeReportLogs() function
 
-
-
+function downloadSecurityReport() {
+	var event = document.createEvent('CustomEvent');
+	event.initCustomEvent("downloadReport-message", true, true, undefined);
+	document.documentElement.dispatchEvent(event);
+} // end of downloadSecurityReport() function
