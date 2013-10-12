2d308b6ae287279437b2d269e5a5aa608011cc1d
Kailas <patilkr@patilkr-K43SD.(none)>
2013-10-13 02:34:57 +0530

    collapse and exapnd button is in progress

--- data/securityReport-new.js
+++ data/securityReport-new.js
@@ -10,6 +10,11 @@ function downloadSecurityReport() {
 	// var event = document.createEvent('CustomEvent');
 	// event.initCustomEvent("downloadReport-message", true, true, "");
 	var event = new CustomEvent("downloadReport-message");
-	dump("\n\n About to dispatch custom event");
+	// dump("\n\n About to dispatch custom event");
 	document.documentElement.dispatchEvent(event);
 } // end of downloadSecurityReport() function
+
+function openSecurityReport() {
+	var event = new CustomEvent("openSecReport-msg");
+	document.documentElement.dispatchEvent(event);
+}
